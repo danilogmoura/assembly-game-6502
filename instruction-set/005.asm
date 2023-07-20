3f7de863957f0250ddc74af1915bfba2c6d04828
Danilo Gomes de Moura <danilogmoura@gmail.com>
2023-07-20 04:17:42 -0300

    Estudo das instruções set

---
+++ instruction-set/005.asm
@@ -0,0 +1,26 @@
+    processor 6502
+    seg Code            ; Defina um novo segmento chamado "Código" 
+    org $F000           ; Defina a origem do código ROM no endereço de memória $F000
+Start:
+    CLD                 ; Certifique-se de que o modo decimal está limpo (desativado)
+    LDA #$0A            ; Carregue o registrador A com o valor hexadecimal $A
+    LDX #%1010          ; Carregue o registrador X com o valor binário %1010
+     
+    STA $80             ; Armazene o valor no registrador A no endereço de memória (página zero) $80
+    STX $81             ; Armazene o valor no registrador X no endereço de memória (página zero) $81
+ 
+    LDA #10             ; Carregue A com o valor decimal 10
+
+    CLC                 ; Limpe o bit de carry
+    ADC $80             ; Adicione a A o valor dentro do endereço RAM $80
+    ADC $81             ; Adicione a A o valor dentro do endereço RAM $81
+
+                        ; A deve conter (#10 + $A + %1010) = #30 (ou $1E em hexadecimal)
+                        
+    STA $82             ; Armazene o valor de A na posição RAM $ 82
+    
+    jmp Start           ; Salte para o endereço de memória $F000 (loop infinito)
+
+    org $FFFC           ; Termine a ROM adicionando os valores necessários à posição de memória $FFFC
+    .word Start         ; Coloque 2 bytes com o endereço de reset na posição de memória $FFFC
+    .word Start         ; Coloque 2 bytes com o endereço de quebra na posição de memória $FFFE
