afba4dca340f375629f3acfd402e06ea940ffb2b
Danilo Gomes de Moura <danilogmoura@gmail.com>
2023-07-21 16:41:23 -0300

    Alterada as cores do placar

--- screenobjects/playfield.asm
+++ screenobjects/playfield.asm
@@ -12,25 +12,28 @@ Reset:
     ldx #$80                    ; background azul
     stx COLUBK
 
-    lda #%1111			; playfiled branco
+    lda #%1111			        ; playfiled branco
     sta COLUPF
  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Setamos a cor do P0 e P1 nos registrados TIA
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    lda #$48			; player 0, cor vermelho
+    lda #$48			        ; player 0, cor vermelho
     sta COLUP0
     
-    lda #$C6			; player 1, cor verde
+    lda #$C6			        ; player 1, cor verde
     sta COLUP1
+
+    ldy #%00000010              ; CTRLPF D1 set to 1 means (score)
+    sty CTRLPF
     
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Novo frame para configurar o VBLANK e o VSYNC
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 StartFrame:
     lda #2
-    sta VBLANK			; ativa o VBLANK
-    sta VSYNC			; ativa o VSYNC
+    sta VBLANK			        ; ativa o VBLANK
+    sta VSYNC			        ; ativa o VSYNC
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Gera 3 linha do VSYNC
@@ -40,7 +43,7 @@ StartFrame:
     REPEND
 
     lda #00
-    sta VSYNC			; desativa o VSYNC
+    sta VSYNC			        ; desativa o VSYNC
             
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Gera as 37 linha de saída recomendadas pelo TIA
@@ -50,7 +53,7 @@ StartFrame:
     REPEND
         
     lda #00
-    sta VBLANK			; desativa o VBLANK
+    sta VBLANK			        ; desativa o VBLANK
         
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Gera as scanlines visíveis
@@ -69,14 +72,14 @@ VisibleScanlines:
 
 ScoreboardLoop:
     lda NumberBitmap,y
-    sta PF1			; ativa o Playfiel 1
-    sta WSYNC
+    sta PF1			            ; ativa o Playfiel 1
+    sta WSYNC       
     iny
     cpy 10
     bne ScoreboardLoop
     
     lda #00
-    sta PF1			; desativa o Playfiel 1
+    sta PF1			            ; desativa o Playfiel 1
     
     ; Desenha 50 linhas entre os players e o placar
     REPEAT 50
@@ -90,14 +93,14 @@ ScoreboardLoop:
 
 Player0Loop:
     lda PlayerBitmap,y
-    sta GRP0			; ativa player 0
+    sta GRP0                        ; ativa player 0
     sta WSYNC
     iny
     cpy #10
     bne Player0Loop
     
     lda #00
-    sta GRP0			; desativa player 0
+    sta GRP0	                    ; desativa player 0
     
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Desenha 10 scanlines para o GRP1 (player 1), usando o array de bytes definido
@@ -107,14 +110,14 @@ Player0Loop:
     
 Player1Loop:
     lda PlayerBitmap,y
-    sta GRP1			; ativa o player 1
+    sta GRP1			            ; ativa o player 1
     sta WSYNC
     iny
     cpy #10
     bne Player1Loop
     
     lda #00
-    sta GRP1 			; desativa o player 1
+    sta GRP1 			            ; desativa o player 1
     
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Desenha o restando das scanlines
@@ -146,36 +149,36 @@ Player1Loop:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     org $FFE8
 PlayerBitmap:
-    .byte #%01111110   		;  ######
-    .byte #%11111111   		; ########
-    .byte #%10011001   		; #  ##  #
-    .byte #%11111111   		; ########
-    .byte #%11111111   		; ########
-    .byte #%11111111   		; ########
-    .byte #%10111101   		; # #### #
-    .byte #%11000011   		; ##    ##
-    .byte #%11111111   		; ########
-    .byte #%01111110   		;  ######
+    .byte #%01111110   		            ;  ######
+    .byte #%11111111   		            ; ########
+    .byte #%10011001   		            ; #  ##  #
+    .byte #%11111111   		            ; ########
+    .byte #%11111111   		            ; ########
+    .byte #%11111111   		            ; ########
+    .byte #%10111101   		            ; # #### #
+    .byte #%11000011   		            ; ##    ##
+    .byte #%11111111   		            ; ########
+    .byte #%01111110   		            ;  ######
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Placar
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     org $FFF2
 NumberBitmap:
-    .byte #%00001110   		; ########
-    .byte #%00001110   		; ########
-    .byte #%00000010   		;      ###
-    .byte #%00000010   		;      ###
-    .byte #%00001110   		; ########
-    .byte #%00001110   		; ########
-    .byte #%00001000   		; ###
-    .byte #%00001000   		; ###
-    .byte #%00001110   		; ########
-    .byte #%00001110   		; ########
+    .byte #%00001110   		            ; ########
+    .byte #%00001110   		            ; ########
+    .byte #%00000010   	            	;      ###
+    .byte #%00000010   	            	;      ###
+    .byte #%00001110   	            	; ########
+    .byte #%00001110   	            	; ########
+    .byte #%00001000   	            	; ###
+    .byte #%00001000   		            ; ###
+    .byte #%00001110   	            	; ########
+    .byte #%00001110   	            	; ########
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Completa a ROM com 4KB, exigência do 6502
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    org $FFFC			; Define a origem para $FFFC
-    .word Reset			; Endereço de reset $FFFC (o 6502 inicia a execução do programa nesse endereço)
-    .word Reset			; Endereço de IRQ (interrupção) $FFFE
+    org $FFFC		                	; Define a origem para $FFFC
+    .word Reset			                ; Endereço de reset $FFFC (o 6502 inicia a execução do programa nesse endereço)
+    .word Reset		                	; Endereço de IRQ (interrupção) $FFFE
